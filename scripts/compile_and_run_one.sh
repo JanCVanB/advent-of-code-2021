b1ee0301d38a4f4d68665eda22027b677b9c1b4a
Jan Van Bruggen <JanCVanB@pm.me>
2021-12-18 19:40:18 -0700

    Improve script docs

--- scripts/compile_and_run_one.sh
+++ scripts/compile_and_run_one.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
-# Note: This script requires that the Roc CLI is installed.
+# Note: This script requires that the Roc CLI is installed
+#	      and that nix-shell is active with rthe oc/shell.nix config.
 
 # Example usage: `./scripts/compile_and_run_one.sh ./solutions/day-1-sonar-sweep/solution_1a.roc`
 
--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,7 +1,6 @@
 #!/bin/zsh
 
-# Note: This script requires that the Roc CLI is installed
-#       and all solutions have already been compiled.
+# Note: This script requires that all solutions have already been compiled.
 
 for solution_module in solutions/**/solution_*.roc; do
 	solution_executable="./${solution_module%.*}"
